4e029c848fcf7904ebe3ed8c6a9848ded4afb778
Steve Lovell <sjlovell34@gmail.com>
2013-09-04 15:39:02 -0400

    add MIT license info

--- parse_caml.js
+++ parse_caml.js
@@ -1,7 +1,7 @@
 
 // The MIT License (MIT)
 
-// Copyright (c) [2013] [Stephen J. Lovell]
+// Copyright (c) 2013 Stephen J. Lovell
 
 // Permission is hereby granted, free of charge, to any person obtaining a copy of
 // this software and associated documentation files (the "Software"), to deal in
